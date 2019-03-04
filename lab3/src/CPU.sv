9188bc1cd67fb3b43b49dade0209b9c8d3c48aa3
luke-jiang <yuxuanlukejiang@gmail.com>
2019-03-04 01:30:58 -0800

    style

--- lab3/src/CPU.sv
+++ lab3/src/CPU.sv
@@ -34,16 +34,16 @@ module CPU (clk, reset);
   logic [63:0] address;           // Current Address
   logic [31:0] instruction;       // Current Instruction
   logic [12:0] cntrl;             // 13-bit control
-  logic [3:0] regfIn, regfOut;    // Data input/output to flag reg
-  logic lessThanF;                // less-than flag
+  logic [3:0]  regfIn, regfOut;   // Data input/output to flag reg
+  logic        lessThanF;         // less-than flag
 
   // input data to CPU
   logic [63:0] Imm12;
   logic [63:0] Imm26, Imm26LS2;
   logic [63:0] Imm9;
   logic [63:0] Imm19;
-  logic [4:0] Rn, Rd, Rm;
-  logic [5:0] shamt;
+  logic [4:0]  Rn, Rd, Rm;
+  logic [5:0]  shamt;
 
   // Connection between modules
   logic [63:0] incr4Adder_out;
@@ -51,7 +51,7 @@ module CPU (clk, reset);
   logic [63:0] BrTakenMux_out;
   logic [63:0] CondBrMux_out;
   logic [63:0] LS2_out;
-  logic [4:0] Reg2LocMux_out;
+  logic [4:0]  Reg2LocMux_out;
   logic [63:0] ALUSrcMux_out;
   logic [63:0] shifter_out;
   logic [63:0] DataMem_out;
@@ -60,10 +60,18 @@ module CPU (clk, reset);
   logic [63:0] ALU_out;
 
   // The Program Counter
-  PC programCounter (.out(address), .in(BrTakenMux_out), .reset, .clk);
+  PC programCounter (
+    .out          (address),
+    .in           (BrTakenMux_out),
+    .reset, .clk
+  );
 
   // The InstructMem
-  instructmem instrm (.address, .instruction, .clk);
+  instructmem instrm (
+    .address,
+    .instruction,
+    .clk
+  );
 
   // Assign CPU inputs based on instruction
   assign Imm12 = {52'b0, instruction[21:10]};
@@ -106,102 +114,116 @@ module CPU (clk, reset);
   end
 
   mux64x2_1 CondBrMux (
-    .out(CondBrMux_out),
-    .d({Imm19, Imm26}),
-    .sel(cntrl[CondBr])
+    .out                (CondBrMux_out),
+    .d                  ({Imm19, Imm26}),
+    .sel                (cntrl[CondBr])
   );
 
    // Left shift Imm26 by 2
   shifter LS2 (
-    .value(CondBrMux_out),
-    .direction(1'b0),
-    .distance(6'd2),
-    .result(LS2_out)
+    .value              (CondBrMux_out),
+    .direction          (1'b0),
+    .distance           (6'd2),
+    .result             (LS2_out)
   );
 
   // Adder for incrementing 4 to current address
   alu incr4Adder (
-    .A(64'd4), .B(address),
-    .cntrl(3'b010),
-    .result(incr4Adder_out),
-    .negative(), .zero(), .overflow(), .carry_out()
+    .A                  (64'd4),
+    .B                  (address),
+    .cntrl              (3'b010),
+    .result             (incr4Adder_out),
+    .negative           (),
+    .zero               (),
+    .overflow           (),
+    .carry_out          ()
   );
 
   // Adder for incrementing specified amount (branch) to current address
   alu branchAdder (
-    .A(LS2_out), .B(address),
-    .cntrl(3'b010),
-    .result(branchAdder_out),
-    .negative(), .zero(), .overflow(), .carry_out()
+    .A                  (LS2_out),
+    .B                  (address),
+    .cntrl              (3'b010),
+    .result             (branchAdder_out),
+    .negative           (),
+    .zero               (),
+    .overflow           (),
+    .carry_out          ()
   );
 
   // Select which address as the next address
   mux64x2_1 BrTakenMux (
-    .out(BrTakenMux_out),
-    .d({branchAdder_out, incr4Adder_out}),
-    .sel(cntrl[BrTaken])
+    .out                (BrTakenMux_out),
+    .d                  ({branchAdder_out, incr4Adder_out}),
+    .sel                (cntrl[BrTaken])
   );
 
   // The RegFile
   mux5x2_1 Reg2LocMux (
-    .out(Reg2LocMux_out),
-    .d({Rm, Rd}),
-    .sel(cntrl[Reg2Loc])
+    .out                (Reg2LocMux_out),
+    .d                  ({Rm, Rd}),
+    .sel                (cntrl[Reg2Loc])
   );
   regfile regfi (
-    .ReadData1, .ReadData2, .WriteData,
-    .ReadRegister1(Rn), .ReadRegister2(Reg2LocMux_out),
-    .WriteRegister(Rd), .RegWrite(cntrl[RegWrite]),
+    .ReadData1,
+    .ReadData2,
+    .WriteData,
+    .ReadRegister1      (Rn),
+    .ReadRegister2      (Reg2LocMux_out),
+    .WriteRegister      (Rd),
+    .RegWrite           (cntrl[RegWrite]),
     .clk
   );
 
   // The ALU
   mux64x4_1 ALUSrcMux (
-    .out(ALUSrcMux_out),
-    .d({64'b0, Imm12, Imm9, ReadData2}),
-    .sel(cntrl[ALUSrc1:ALUSrc0])
+    .out                (ALUSrcMux_out),
+    .d                  ({64'b0, Imm12, Imm9, ReadData2}),
+    .sel                (cntrl[ALUSrc1:ALUSrc0])
   );
   alu theALU (
-    .A(ReadData1), .B(ALUSrcMux_out),
-    .cntrl(cntrl[ALUOp1:ALUOp0]),
-    .result(ALU_out),
-    .negative(regfIn[negativeF]),
-    .zero(regfIn[zeroF]),
-    .overflow(regfIn[overflowF]),
-    .carry_out(regfIn[carry_outF])
+    .A                  (ReadData1),
+    .B                  (ALUSrcMux_out),
+    .cntrl              (cntrl[ALUOp1:ALUOp0]),
+    .result             (ALU_out),
+    .negative           (regfIn[negativeF]),
+    .zero               (regfIn[zeroF]),
+    .overflow           (regfIn[overflowF]),
+    .carry_out          (regfIn[carry_outF])
   );
 
   // The flag register
   regflag regfl (
-    .out(regfOut),
+    .out                (regfOut),
     .lessThanF,
-    .in(regfIn),
-    .en(cntrl[FlagWrite]),
+    .in                 (regfIn),
+    .en                 (cntrl[FlagWrite]),
     .reset, .clk
   );
 
   // The shifter
   shifter theRightShifter (
-    .result(shifter_out),
-    .value(ReadData1),
-    .direction(1'b1),
-    .distance(shamt)
+    .result             (shifter_out),
+    .value              (ReadData1),
+    .direction          (1'b1),
+    .distance           (shamt)
   );
 
   // The DataMem
   datamem datam (
-    .address(ALU_out),
-    .write_enable(cntrl[MemWrite]),
-    .read_enable(cntrl[Sel2Reg1]),
-    .write_data(ReadData2),
-    .read_data(DataMem_out),
-    .clk, .xfer_size(4'd8)
+    .address            (ALU_out),
+    .write_enable       (cntrl[MemWrite]),
+    .read_enable        (cntrl[Sel2Reg1]),
+    .write_data         (ReadData2),
+    .read_data          (DataMem_out),
+    .xfer_size          (4'd8),
+    .clk
   );
 
   mux64x4_1 Sel2RegMux (
-    .out(WriteData),
-    .d({64'b0, DataMem_out, shifter_out, ALU_out}),
-    .sel(cntrl[Sel2Reg1:Sel2Reg0])
+    .out                (WriteData),
+    .d                  ({64'b0, DataMem_out, shifter_out, ALU_out}),
+    .sel                (cntrl[Sel2Reg1:Sel2Reg0])
   );
 
 endmodule
